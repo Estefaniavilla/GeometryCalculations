d2a3c8022283b3d26925a92562ddfb2fcc8321a4
estefivilla07@gmail.com <estefivilla07@gmail.com>
2024-02-27 21:05:39 -0600

    Agregar archivos de proyecto.

---
+++ GeometryCalculations/Program.cs
@@ -0,0 +1,28 @@
+﻿using GeometryCalculations;
+using System;
+
+namespace CalculoPerimetroTriangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Triangle myTriangle = new Triangle();
+
+
+            Console.Write("Enter the length of the first side: ");
+            double side1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter the length of the second side: ");
+            double side2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter the length of the third side: ");
+            double side3 = Convert.ToDouble(Console.ReadLine());
+
+            double perimeter = myTriangle.calculateperimeter(side1, side2, side3);
+
+            Console.WriteLine($"The perimeter of the triangle is: {perimeter}");
+        }
+    }
+}
---
+++ GeometryCalculations/Triangle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryCalculations
+{
+    internal class Triangle
+        
+    {
+            public double calculateperimeter (double a, double b, double c)
+                
+            {
+                return a + b + c;
+            }
+        }
+    }
+
+
+
+
